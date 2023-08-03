b55a6af8fe248c9a3f9b823881342035b1012174
Lucas David Erkana <lderkana21x@gmail.com>
2023-08-03 19:45:08 +0200

    Add Home page

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
-// src/App.js
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
-import NavBarMobile from './components/NavBarMobile';
 import AllPricing from './pages/AllPricing';
+import Home from './components/Home';
 import Features from './components/Features';
 import Clients from './components/Clients';
 import FAQ from './components/FAQ';
 
 function App() {
-  const [isMobile, setIsMobile] = useState(false);
-
-  const checkIsMobile = () => {
-    setIsMobile(window.innerWidth <= 768); // Adjust the breakpoint as needed
-  };
-
-  useEffect(() => {
-    checkIsMobile();
-    window.addEventListener('resize', checkIsMobile);
-    return () => {
-      window.removeEventListener('resize', checkIsMobile);
-    };
-  }, []);
-
   return (
     <div className="App">
-      {isMobile ? <NavBarMobile /> : <NavBar />}
+      <NavBar />
+      <Home /> {/* Include the Home component */}
       <AllPricing />
       <Features />
       <Clients />
---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <section className="section-lg home-alt bg-gradient-1" id="home">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-lg-8">
+            <div className="home-wrapper text-center">
+              <p className="text-muted">Creative Landing Page Template</p>
+              <h1>Here is the best way to present your apps</h1>
+              <a href="#" className="btn btn-primary active">
+                Get Started
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
