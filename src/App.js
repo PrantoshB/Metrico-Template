924c66508f1e3acf1b6f7ef17f20de37d0cac104
Prantosh Biswas <pbiswas1007@gmail.com>
2023-08-03 20:13:44 +0530

    Create Desktop Version

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
+import Footer from './components/Footer';
 import NavBar from './components/NavBar';
 import AllPricing from './pages/AllPricing';
+import Faq from './pages/Faq';
+import Features from './pages/Features';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Home/>
+      <Features/>
       <AllPricing/>
+      <Faq/>
+      <Footer/>
     </div>
   );
 }
